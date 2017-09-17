4e9b7bb93adbf97e3b2790efb5d716912f266775
Eric Han <erichan@Erics-MacBook-Pro.local>
2017-09-16 19:28:00 -0700

    fixed implementation to match professors pseudocode in class

--- src/Convergecast/Convergecast.java
+++ src/Convergecast/Convergecast.java
@@ -1,44 +1,64 @@
 package Convergecast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Convergecast {
 	public static Processor root;
 	ArrayList<Processor> processors = new ArrayList<Processor>();
-	public int findMax(int[] values) {
-		int max = 0;
-		return max;
-	}
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Inside Convergecast main method");
 		Convergecast cc = new Convergecast();
 		cc.init();
 	}
 	
 	public void init() {
-		Processor p0 = new Processor(0,2);
-		Processor p1 = new Processor(1,1);
-		Processor p2 = new Processor(2,3);
+		Processor p0 = new Processor(0,101111111);
+		Processor p1 = new Processor(1,100000000);
+		Processor p2 = new Processor(2,99);
+		Processor p3 = new Processor(3,999999);
 		processors.add(p0);
 		processors.add(p1);
 		processors.add(p2);
-		p0.children.add(p1);
-		p1.children.add(p2);
+		processors.add(p3);
+		p0.left = p1;
+		p1.left = p2;
+		p2.left = p3;
+		p1.parent = p0;
+		p2.parent = p1;
+		p3.parent = p2;
+		p0.parent = null;
 		for(Processor p : processors) {
 			setLeafNode(p);
+			setRoot(p);
 		}
-		for(Processor p : processors) {
-			if(p.isLeafNode) {
-				p.returnMax(p.max);
-			}
-		}
+		findMax(root);
 	}
 	
+	public int findMax(Processor node) {
+		if(node==null) return Integer.MIN_VALUE;
+		else return Max(node.value, findMax(node.left), findMax(node.right));
+	}
+	
+	public int Max(int nodevalue, int leftvalue, int rightvalue) {
+		ArrayList<Integer> maxValues = new ArrayList<Integer>();
+		maxValues.add(nodevalue);
+		maxValues.add(leftvalue);
+		maxValues.add(rightvalue);
+		int max = Collections.max(maxValues);
+		System.out.println("Maximum value found is: " + max);
+		return max;
+	}
+	
+	public void setRoot(Processor p) {
+		if(p.parent==null) {
+			root = p;
+			p.isRoot = true;
+		}
+	}
     public void setLeafNode(Processor p) {
-        if(p.children.size() >0) p.isLeafNode = false;
+    		if(p.left == null && p.right == null) p.isLeafNode = true;
     }
-    
-    
-    
+     
 }
--- src/Convergecast/Processor.java
+++ src/Convergecast/Processor.java
@@ -1,43 +1,20 @@
 package Convergecast;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by tphadke on 8/29/17.
- */
 public class Processor {
 	boolean isLeafNode = true;
     int id ;
     int value;
-    int max;
-    List<Processor> children ;
+    boolean isRoot = false;
+    Processor left;
+    Processor right;
     Processor parent;
     
-    public Processor(int id, int value) {
+    public Processor(int id, int val) {
     		this.id = id;
-    		this.value = value;
-        children = new ArrayList<Processor>();
+    		value = val;
+    		parent = null;
+    		left = null;
+    		right = null;
     }
 
-    public int findMax(List<Processor> children) {
-		ArrayList<Processor> children2 = new ArrayList<Processor>();
-    		if(children.size() == 0 || children == null) {
-    			return value;
-    		}
-    		for(Processor c : children) {
-    			if (c.value> max) {
-    				this.max = c.value;
-    			}
-			returnMax();
-    			children2 = (ArrayList<Processor>) c.children;
-    			findMax(children2);
-    		}
-    		return max;
-    }
-    
-    public void returnMax() {
-    		parent.max = max;
-	}
-    
 }
